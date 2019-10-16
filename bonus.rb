99ab39d5b60bb790c98864b43f94b834482ae3b1
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-10-16 03:20:09 +0000

    Initial commit

---
+++ bonus.rb
@@ -0,0 +1,32 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def infinite_list?(id = nil)
+    return true if id == object_id
+    return false if next_node.nil?
+    return next_node.infinite_list?(id || object_id)
+  end
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+node1.next_node = node3
+
+# print_values(node3)
+puts node1.infinite_list?
